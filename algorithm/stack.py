5f2a4a69f7ad8852d14bb020e65ac7f040399f66
youngchan <san0558@naver.com>
2022-04-29 08:07:53 +0900

    Add stack.py
    - 스택 문제 유형 추가

---
+++ algorithm/stack.py
@@ -0,0 +1,17 @@
+"""
+type_1
+빵과 음료수같이 짝이 있는 문제에 사용.
+음료수를 받았는데 빵을 못 받은 경우를 구할 때 사가
+"""
+
+
+def solution_type_1(s):
+    c = 0
+    for x in s:
+        if x == '빵':
+            c += 1
+        else:
+            c -= 1
+        if c < 0:
+            return False
+    return c == 0
