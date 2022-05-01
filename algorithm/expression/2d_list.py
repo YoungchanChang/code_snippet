9c0b1f02e1379e1825c5e967f195c5bfc4c366ed
youngchan <san0558@naver.com>
2022-05-01 09:19:32 +0900

    Add 2d_list.py
    - 2차원 배열 코드 스니펫 추가

---
+++ algorithm/expression/2d_list.py
@@ -0,0 +1,10 @@
+
+
+if __name__ == "__main__":
+
+    ROW = 3
+    COLUMN = 4
+
+    for EACH_ROW in range(0, ROW, 1):
+        for EACH_COLUMN in range(0, COLUMN, 1):
+            print(EACH_ROW, EACH_COLUMN)
