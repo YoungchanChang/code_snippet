38d1f6503f77cb428f76be1dc82f51ddbdb77dde
youngchan <san0558@naver.com>
2022-05-03 18:09:05 +0900

    Add connection_pool.py
    - 커넥션 풀 방식 예시 추가

---
+++ database/mysql/connection_pool.py
@@ -0,0 +1,57 @@
+"""
+connection pool : https://docs.sqlalchemy.org/en/14/core/pooling.html
+session : https://docs.sqlalchemy.org/en/14/orm/session_basics.html
+"""
+
+import time
+import pymysql
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from database.mysql import config_db
+from faker import Faker
+
+user_analyzed_engine = create_engine(f'mysql://{config_db.TEST_DB_INFO.get("user")}:{config_db.TEST_DB_INFO.get("password")}@{config_db.TEST_DB_INFO.get("host")}:{config_db.TEST_DB_INFO.get("port")}/{config_db.TEST_DB_INFO.get("db")}', convert_unicode=False, pool_size=20, pool_recycle=500, max_overflow=20)
+
+fake = Faker()
+
+
+def get_connection():
+    MYSQL_CONN = pymysql.connect(
+        **config_db.TEST_DB_INFO
+    )
+    if not MYSQL_CONN.open:
+        MYSQL_CONN.ping(reconnect=True)
+
+    return MYSQL_CONN
+
+
+def create_user_by_single_conn():
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        SQL = f"""
+        INSERT INTO user_db (user_name, user_email) VALUES ('{fake.name()}', '{fake.ascii_safe_email()}');
+        """
+        cursor.execute(SQL)
+    conn.commit()
+
+
+def create_user_by_pool():
+    with Session(autocommit=False, autoflush=False, bind=user_analyzed_engine) as session:
+        SQL = f"INSERT INTO user_db (user_name, user_email) VALUES ('{fake.name()}', '{fake.ascii_safe_email()}');"
+        row_count = session.execute(SQL).rowcount
+        session.commit()
+        return row_count
+
+
+if __name__ == "__main__":
+    st = time.process_time()
+
+    for i in range(0, 1000, 1):
+        create_user_by_single_conn()
+    print("not_connection_pool", time.process_time()-st)
+
+    st = time.process_time()
+    for i in range(0, 1000, 1):
+        create_user_by_pool()
+
+    print("connection_pool", time.process_time() - st)
