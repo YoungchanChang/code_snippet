f5b05fe4af48b81aa2f4cb22e601d56312da249b
youngchan <san0558@naver.com>
2022-05-04 18:16:00 +0900

    Update 2d_list.py
    - 2차원 리스트 추가

--- algorithm/expression/2d_list.py
+++ algorithm/expression/2d_list.py
@@ -1,10 +1,21 @@
 
 
-if __name__ == "__main__":
+def create_empty_sec_array():
+    """빈 2차원 배열 생성"""
+    lst = [[0] * 5 for __ in range(5)]
+    print(lst) # [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 
+
+def check_sec_array():
+    """2차원 리스트 행과 열로 읽기"""
     row = 3
     column = 4
 
-    for each_row in range(0, row, 1):
+    for each_row in range(0, row, 1): # step이 1이면 생략 가능
         for each_column in range(0, column, 1):
             print(each_row, each_column)
+
+
+if __name__ == "__main__":
+
+    ...
