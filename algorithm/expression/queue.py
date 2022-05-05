1c98cf50eb609bfdd2e7434c32dfd29329a919bb
youngchan <san0558@naver.com>
2022-05-06 07:39:10 +0900

    Update queue.py
    - 큐 자료구조 문제 유형 정리

--- algorithm/expression/queue.py
+++ algorithm/expression/queue.py
@@ -1,10 +1,20 @@
+"""
+
+"""
+
 from collections import deque
+
+
+def get_que_template():
+    image_que = deque(image)
+
+    while image_que:
+        image_que_item = image_que.popleft() # 큐를 하나 뽑는다.
+
+        if image_que_item < 1: # 큐에 조건문을 건다.
+            image_que.append(image_que_item) # 큐에 조건을 추가한다.
+
 
 if __name__ == "__main__":
     image = [0, 1, 2]
-    image_que = deque(image)
-    tmp_que = []
-
-    while image_que:
-        image_que_item = image_que.popleft()
-        print(image_que_item)
+    get_que_template()
