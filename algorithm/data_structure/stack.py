092b39c773b9729e253f9dd806fe521656ccf034
youngchan <san0558@naver.com>
2022-05-07 07:22:04 +0900

    Update stack.py
    - 스택 자료구조 함수 이름 변경

--- algorithm/data_structure/stack.py
+++ algorithm/data_structure/stack.py
@@ -1,11 +1,11 @@
 """
-type_1
+get_even_data()
 빵과 음료수같이 짝이 있는 문제에 사용.
 음료수를 받았는데 빵을 못 받은 경우를 구할 때 사가
 """
 
 
-def solution_type_1(s):
+def get_even_data(s):
     c = 0
     for x in s:
         if x == '빵':
