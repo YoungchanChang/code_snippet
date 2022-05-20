0eff0add5a27dfb239e69dd24c7e8f887d027c71
youngchan <san0558@naver.com>
2022-05-20 09:07:52 +0900

    Add mysql, mongodb, config_db in basic_mvc
    - mysql, mongodb, config_db 설정 추가

---
+++ flask_factory/basic_mvc/model/config_db.py
@@ -0,0 +1,7 @@
+TEST_DB_INFO = {
+    'host': '127.0.0.1',
+    'db': 'test',
+    'user': 'root',
+    'password': 'test',
+    'port': 3306
+}
---
+++ flask_factory/basic_mvc/model/mysql.py
@@ -0,0 +1,34 @@
+import pymysql
+from flask_factory.basic_mvc.model import config_db
+
+
+def get_connection():
+    MYSQL_CONN = pymysql.connect(
+        **config_db.TEST_DB_INFO
+    )
+    if not MYSQL_CONN.open:
+        MYSQL_CONN.ping(reconnect=True)
+
+    return MYSQL_CONN
+
+
+def select_all():
+    """모든 데이터 반환"""
+    test_db_conn = get_connection()
+    with test_db_conn.cursor() as cursor:
+        TABLE = "user_db"
+        sql = f"SELECT * FROM {TABLE};"
+        return cursor.fetchall(sql)
+
+
+def insert_values(user_name, user_email):
+    """회원가입 요청"""
+    test_db_conn = get_connection()
+    with test_db_conn.cursor() as cursor:
+        TABLE = "user_db"
+        sql = f"INSERT INTO {TABLE} (user_name, user_email) VALUES ('{user_name}', '{user_email}');"
+        cursor.fetchall(sql)
+        results = test_db_conn.affected_rows()
+
+    test_db_conn.close()
+    return results
