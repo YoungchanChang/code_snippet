bdbe46bafcd417fcdc3a50094761071da2adb5ce
youngchan <san0558@naver.com>
2022-05-20 10:31:20 +0900

    Add app.py
    - 플라스크 팩토리 앱 추가

---
+++ flask_factory/basic_mvc/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from view import user_api
+
+app = Flask(__name__)
+app.register_blueprint(user_api.user_info, url_prefix='/user_info')
+
+
+@app.errorhandler(404)  # 없는 페이지를 요청했을 때의 에러
+def page_not_found(error):
+    return "<h1>404 Error</h1>", 404
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
+
