4f0be1ebd8dcdf0b3669265e0b98a8eacc758387
youngchan <san0558@naver.com>
2022-05-20 09:12:11 +0900

    Update config_db.py
    - 운영 경로에 따라 TEST_DB_INFO 다르게 적용할 수 있ㄴ느 코드 추가

--- flask_factory/basic_mvc/model/config_db.py
+++ flask_factory/basic_mvc/model/config_db.py
@@ -1,7 +1,23 @@
-TEST_DB_INFO = {
-    'host': '127.0.0.1',
-    'db': 'test',
-    'user': 'root',
-    'password': 'test',
-    'port': 3306
-}
+import platform
+
+
+def get_dev_op():
+    if platform.system() == "Darwin":
+        TEST_DB_INFO = {
+            'host': '127.0.0.1',
+            'db': 'test',
+            'user': 'root',
+            'password': 'test',
+            'port': 3306
+        }
+    else: # 운영서버인 경우 환경설정
+
+        TEST_DB_INFO = {
+            'host': '127.0.0.1',
+            'db': 'test',
+            'user': 'root',
+            'password': 'test',
+            'port': 3306
+        }
+
+    return TEST_DB_INFO
